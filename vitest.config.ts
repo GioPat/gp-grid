fcdd60190364c371e3b4975bb364d26c308ed2f6
GioPat <giovanni.patruno@outlook.com>
2026-03-20 18:41:22 +0100

    cicd: add sonarqube checks

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     coverage: {
       provider: "v8",
-      exclude: ["playgrounds/**", "benchmarks/**", "**/**.bench.ts", "**/*.config.ts"],
+      reporter: ["html", "lcov"],
+      exclude: [...configDefaults.exclude, "playgrounds/**", "benchmarks/**", "**/**.bench.ts", "**/*.config.ts"],
     },
     projects: [
       "packages/core",
